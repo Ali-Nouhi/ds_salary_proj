f9e8126361c1671fd2d705919efe227121ffc1ef
AliNouhi <alinouhi@gmail.com>
2020-09-27 16:16:13 -0400

    uploadig data and datacollection script

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -194,3 +194,13 @@ def get_jobs(keyword, num_jobs, verbose,path,slp_time):
             break
 
     return pd.DataFrame(jobs)  #This line converts the dictionary object into a pandas DataFrame.
+
+
+# Run script:
+
+#import glassdoor_scraper as gs
+# import pandas as pd
+# path = "C:/Users/Mollasadra/Documents/ds_salary_proj/chromedriver"
+
+# df = gs.get_jobs('data scientist',15,False, path, 15)
+# df  
